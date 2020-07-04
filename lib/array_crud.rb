ac8c8eeb3a6e9a4502e2ab970e88fd45caf83f28
Morgan Matney <morgan.matney89@gmail.com>
2020-07-04 15:06:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [name, age, birth_date, location]
+  ["first", "second", "third", "fourth"]
 end
 
 def add_element_to_end_of_array(array, element)
